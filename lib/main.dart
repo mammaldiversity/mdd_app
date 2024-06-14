45c0059e30bc235169edfc8b4c7f0baee7391a1b
Heru Handika <herubiolog@gmail.com>
2024-06-14 11:47:45 -0500

    Add color schemes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test_mdd/screens/home/home.dart';
 import 'package:test_mdd/src/rust/frb_generated.dart';
+import 'package:test_mdd/styles/themes.dart';
 
 Future<void> main() async {
   await RustLib.init();
@@ -12,11 +15,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('MDD')),
-        body: const Center(child: Text('Test')),
-      ),
-    );
+    return DynamicColorBuilder(builder: (lightColorScheme, darkColorScheme) {
+      return MaterialApp(
+        title: 'Mammal Diversity Database',
+        theme: MddTheme.lightTheme(lightColorScheme),
+        darkTheme: MddTheme.darkTheme(darkColorScheme),
+        home: const HomeScreen(),
+      );
+    });
   }
 }
