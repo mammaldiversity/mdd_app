72822fe4585ed04d4e3b030b94ef5192cb555d78
Heru Handika <herubiolog@gmail.com>
2024-06-16 19:18:50 -0500

    Make species more obvious.

--- lib/screens/explore/form.dart
+++ lib/screens/explore/form.dart
@@ -36,82 +36,117 @@ class TaxonForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
+    return Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text('MDD ID: ${taxonData.id}'),
-            SpeciesTextView(taxonData: taxonData),
-            const SizedBox(height: 4),
-            Text(
-              '${taxonData.mainCommonName}',
-              style: Theme.of(context).textTheme.titleMedium?.apply(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-            ),
-            const SizedBox(height: 8),
-            ContentText(
-              title: 'Authority citation',
-              content: taxonData.authoritySpeciesCitation,
-            ),
-            ContentText(
-              title: 'Authority publication link',
-              content: taxonData.authoritySpeciesLink,
-              isUrl: true,
-            ),
-            ContentText(
-              title: 'Original name as described',
-              content: taxonData.originalNameCombination?.replaceAll('_', ' '),
-              isItalic: true,
-            ),
-            ContentText(
-              title: 'Nominal name',
-              content: taxonData.nominalNames,
-            ),
-            ContentText(
-              title: 'Other common names',
-              content: taxonData.otherCommonNames,
-            ),
-            ContentText(
-              title: 'Holotype voucher catalogue number',
-              content: taxonData.holotypeVoucher,
-            ),
-            ContentText(
-              title: 'Type locality',
-              content: taxonData.typeLocality,
-            ),
-            ContentText(
-              title: 'Country distribution',
-              content: taxonData.countryDistribution,
-            ),
-            ContentText(
-              title: 'Distribution notes',
-              content: taxonData.distributionNotes,
-            ),
-            ContentText(
-              title: 'Distribution references',
-              content: taxonData.distributionNotesCitation,
-            ),
-            if (taxonData.taxonomyNotes != 'NA')
-              ContentText(
-                title: 'Taxonomy notes',
-                content: taxonData.taxonomyNotes,
-              ),
-            if (taxonData.taxonomyNotesCitation != 'NA')
-              ContentText(
-                title: 'Taxonomy notes citation',
-                content: taxonData.taxonomyNotesCitation,
-              ),
-            ContentText(
-              title: 'IUCN Red List status',
-              content: matchIUCNStatus(taxonData.iucnStatus),
-            ),
+            SpeciesDetails(taxonData: taxonData),
+            Expanded(
+              child: OtherDetails(taxonData: taxonData),
+            )
           ],
-        ),
+        ));
+  }
+}
+
+class SpeciesDetails extends StatelessWidget {
+  const SpeciesDetails({super.key, required this.taxonData});
+
+  final TaxonomyData taxonData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+      child: Column(
+        children: <Widget>[
+          SpeciesTextView(taxonData: taxonData),
+          const SizedBox(height: 4),
+          Text(
+            '${taxonData.mainCommonName}',
+            style: Theme.of(context).textTheme.titleMedium?.apply(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+          const SizedBox(height: 8),
+          const Divider(),
+        ],
       ),
+    );
+  }
+}
+
+class OtherDetails extends StatelessWidget {
+  const OtherDetails({super.key, required this.taxonData});
+
+  final TaxonomyData taxonData;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+        const SizedBox(height: 8),
+        Center(
+          child: Text('MDD ID: ${taxonData.id}'),
+        ),
+        ContentText(
+          title: 'Authority citation',
+          content: taxonData.authoritySpeciesCitation,
+        ),
+        ContentText(
+          title: 'Authority publication link',
+          content: taxonData.authoritySpeciesLink,
+          isUrl: true,
+        ),
+        ContentText(
+          title: 'Original name as described',
+          content: taxonData.originalNameCombination?.replaceAll('_', ' '),
+          isItalic: true,
+        ),
+        ContentText(
+          title: 'Nominal names',
+          content: taxonData.nominalNames,
+        ),
+        ContentText(
+          title: 'Other common names',
+          content: taxonData.otherCommonNames,
+        ),
+        ContentText(
+          title: 'Holotype voucher catalogue number',
+          content: taxonData.holotypeVoucher,
+        ),
+        ContentText(
+          title: 'Type locality',
+          content: taxonData.typeLocality,
+        ),
+        ContentText(
+          title: 'Country distribution',
+          content: taxonData.countryDistribution,
+        ),
+        ContentText(
+          title: 'Distribution notes',
+          content: taxonData.distributionNotes,
+        ),
+        ContentText(
+          title: 'Distribution references',
+          content: taxonData.distributionNotesCitation,
+        ),
+        if (taxonData.taxonomyNotes != 'NA')
+          ContentText(
+            title: 'Taxonomy notes',
+            content: taxonData.taxonomyNotes,
+          ),
+        if (taxonData.taxonomyNotesCitation != 'NA')
+          ContentText(
+            title: 'Taxonomy notes citation',
+            content: taxonData.taxonomyNotesCitation,
+          ),
+        ContentText(
+          title: 'IUCN Red List status',
+          content: matchIUCNStatus(taxonData.iucnStatus),
+        ),
+      ],
     );
   }
 }
@@ -130,11 +165,15 @@ class SpeciesTextView extends StatelessWidget {
           children: <TextSpan>[
             TextSpan(
               text: speciesText.speciesName,
-              style: Theme.of(context).textTheme.titleMedium?.apply(
+              style: Theme.of(context).textTheme.titleLarge?.apply(
+                    fontFamily: 'Libre Baskerville',
                     fontStyle: FontStyle.italic,
+                    fontWeightDelta: 1,
+                    fontSizeDelta: 4,
+                    letterSpacingDelta: 1,
                   ),
             ),
-            const TextSpan(text: ' '),
+            const TextSpan(text: '\n'),
             TextSpan(
               text: speciesText.speciesAuthorCitation,
               style: Theme.of(context).textTheme.titleMedium,
