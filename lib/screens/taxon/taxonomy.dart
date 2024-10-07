f8083ad65abe4278a7df748927a4220855688c3b
Heru Handika <herubiolog@gmail.com>
2024-10-07 13:16:00 -0500

    Clean taxonomy box.

--- lib/screens/taxon/taxonomy.dart
+++ lib/screens/taxon/taxonomy.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mdd/services/database/database.dart';
+import 'package:mdd/services/system.dart';
 
 class ClassificationContainer extends StatelessWidget {
   const ClassificationContainer({super.key, required this.taxonData});
@@ -8,9 +9,11 @@ class ClassificationContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-      constraints: const BoxConstraints(maxWidth: 400),
+    final ScreenType screenType = getScreenType(context);
+    return Center(
+        child: Container(
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+      constraints: const BoxConstraints(maxWidth: 560),
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.tertiary.withAlpha(32),
         borderRadius: const BorderRadius.all(Radius.circular(16)),
@@ -26,57 +29,112 @@ class ClassificationContainer extends StatelessWidget {
               style: Theme.of(context).textTheme.titleMedium,
             ),
           ),
-          const SizedBox(height: 4),
-          ClassificationData(
-            rank: 'Subclass',
-            name: taxonData.subclass,
-          ),
-          ClassificationData(
-            rank: 'Infraclass',
-            name: taxonData.infraclass,
-          ),
-          ClassificationData(
-            rank: 'Magnorder',
-            name: taxonData.magnorder,
-          ),
-          ClassificationData(
-            rank: 'Superorder',
-            name: taxonData.superorder,
-          ),
-          ClassificationData(
-            rank: 'Order',
-            name: taxonData.taxonOrder,
-          ),
-          ClassificationData(
-            rank: 'Suborder',
-            name: taxonData.suborder,
-          ),
-          ClassificationData(
-            rank: 'Infraorder',
-            name: taxonData.infraorder,
-          ),
-          ClassificationData(
-            rank: 'Parvorder',
-            name: taxonData.parvorder,
-          ),
-          ClassificationData(
-            rank: 'Superfamily',
-            name: taxonData.superfamily,
-          ),
-          ClassificationData(
-            rank: 'Family',
-            name: taxonData.family,
-          ),
-          ClassificationData(
-            rank: 'Subfamily',
-            name: taxonData.subfamily,
-          ),
-          ClassificationData(
-            rank: 'Tribe',
-            name: taxonData.tribe,
-          ),
+          const SizedBox(height: 8),
+          screenType == ScreenType.small
+              ? Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SubOrderAndHigherTaxa(taxonData: taxonData),
+                    LowerInfraOrderTaxa(taxonData: taxonData),
+                  ],
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SubOrderAndHigherTaxa(taxonData: taxonData),
+                    LowerInfraOrderTaxa(taxonData: taxonData),
+                  ],
+                ),
         ],
       ),
+    ));
+  }
+}
+
+class SubOrderAndHigherTaxa extends StatelessWidget {
+  const SubOrderAndHigherTaxa({super.key, required this.taxonData});
+
+  final TaxonomyData taxonData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClassificationData(
+          rank: 'Subclass',
+          name: taxonData.subclass,
+        ),
+        ClassificationData(
+          rank: 'Infraclass',
+          name: taxonData.infraclass,
+        ),
+        ClassificationData(
+          rank: 'Magnorder',
+          name: taxonData.magnorder,
+        ),
+        ClassificationData(
+          rank: 'Superorder',
+          name: taxonData.superorder,
+        ),
+        ClassificationData(
+          rank: 'Order',
+          name: taxonData.taxonOrder,
+        ),
+        ClassificationData(
+          rank: 'Suborder',
+          name: taxonData.suborder,
+        ),
+      ],
+    );
+  }
+}
+
+class LowerInfraOrderTaxa extends StatelessWidget {
+  const LowerInfraOrderTaxa({super.key, required this.taxonData});
+
+  final TaxonomyData taxonData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClassificationData(
+          rank: 'Infraorder',
+          name: taxonData.infraorder,
+        ),
+        ClassificationData(
+          rank: 'Parvorder',
+          name: taxonData.parvorder,
+        ),
+        ClassificationData(
+          rank: 'Superfamily',
+          name: taxonData.superfamily,
+        ),
+        ClassificationData(
+          rank: 'Family',
+          name: taxonData.family,
+        ),
+        ClassificationData(
+          rank: 'Subfamily',
+          name: taxonData.subfamily,
+        ),
+        ClassificationData(
+          rank: 'Tribe',
+          name: taxonData.tribe,
+        ),
+        ClassificationData(
+          rank: 'Genus',
+          name: taxonData.genus,
+          isItalic: true,
+        ),
+      ],
     );
   }
 }
@@ -86,10 +144,12 @@ class ClassificationData extends StatelessWidget {
     super.key,
     required this.rank,
     required this.name,
+    this.isItalic = false,
   });
 
   final String rank;
   final String? name;
+  final bool isItalic;
 
   @override
   Widget build(BuildContext context) {
@@ -105,7 +165,12 @@ class ClassificationData extends StatelessWidget {
                 ),
                 TextSpan(
                   text: name,
-                  style: Theme.of(context).textTheme.bodyMedium,
+                  style: isItalic
+                      ? Theme.of(context).textTheme.bodyMedium?.apply(
+                            fontStyle: FontStyle.italic,
+                            letterSpacingDelta: 0.8,
+                          )
+                      : Theme.of(context).textTheme.bodyMedium,
                 ),
               ],
             ))
