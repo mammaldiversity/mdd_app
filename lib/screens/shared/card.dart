fe2b23a1bafb071f5db79b5d75828047f05076db
Heru Handika <herubiolog@gmail.com>
2026-06-10 14:05:33 -0400

    Allow to show description.

--- lib/screens/shared/card.dart
+++ lib/screens/shared/card.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CommonCard extends StatelessWidget {
-  const CommonCard({super.key, required this.title, required this.child});
+  const CommonCard({
+    super.key,
+    required this.title,
+    required this.child,
+    this.description,
+  });
 
   final String title;
+  final String? description;
   final Widget child;
 
   @override
@@ -21,6 +27,16 @@ class CommonCard extends StatelessWidget {
             padding: const EdgeInsets.only(left: 16),
             child: Text(title, style: Theme.of(context).textTheme.titleMedium),
           ),
+          if (description != null && description!.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text(description!,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  )),
+            ),
           const SizedBox(height: 8),
           child,
         ],
