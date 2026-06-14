11ac77d0746d4eba5751e13104b554e4d2643088
Heru Handika <herubiolog@gmail.com>
2026-06-13 20:16:34 -0400

    Fix the source code URL.

--- lib/services/essential_url.dart
+++ lib/services/essential_url.dart
@@ -2,4 +2,4 @@ const mddWebsiteUrl = 'https://mammaldiversity.org';
 const milUrl = 'https://www.mammalsociety.org/image-library';
 const asmUrl = 'https://www.mammalsociety.org/';
 const mddGitHub = 'https://github.com/mammaldiversity';
-const appSourceCode = 'https://github.com/hhandika/mdd_app';
+const appSourceCode = 'https://github.com/mammaldiversity/mdd_app';
